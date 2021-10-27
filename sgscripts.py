766a86ac436b7fa99c53b10bfcbfe908135ac309
Chuck Jungmann <chuck@cpjj.net>
2021-10-27 08:28:31 -0500

    Add functions, reorder parameters of printing methods.

--- sgscripts.py
+++ sgscripts.py
@@ -159,7 +159,7 @@ class SGScripter:
         space_count = indent_len - len(indented_string)
         print ( (' ' * space_count) + indented_string, end=end)
 
-    def print_proc_list(self, table_name, proc_name, fields):
+    def print_proc_list(self, fields, table_name, proc_name):
 
         autonumber_field = self.get_autonumber_primary_key(fields)
         
@@ -197,14 +197,15 @@ class SGScripter:
             print("END " + self.delimiter)
         
         
-    def print_proc_add(self, table_name, proc_name, fields, confirm_proc_name=None):
+    def print_proc_add(self, fields, table_name, proc_name, confirm_proc_name=None):
         """ Print out conventional Proc_Add procedure that adds
         a new record and if `confirm_proc_name` is not None,
         a call to `confirm_proc_name` with the INSERT_ID() value.
 
         Args:
-           proc_name (string):         Full name of the procedure
            fields    (array):          Collection of field description dictionaries
+           table_name (string):        Name of table for which procedure is created
+           proc_name (string):         Full name of the procedure
            confirm_proc_name (string): Name of procedure to call upon successful
                                        appending of a new record
 
@@ -237,7 +238,7 @@ class SGScripter:
             
         print("END " + self.delimiter)
 
-    def print_proc_read(self, table_name, proc_name, fields):
+    def print_proc_read(self, fields, table_name, proc_name):
         autonumber_field = self.get_autonumber_primary_key(fields)
         
         if autonumber_field is None:
@@ -268,7 +269,7 @@ class SGScripter:
         
             print("END " + self.delimiter)
 
-    def print_proc_update(self, table_name, proc_name, fields, confirm_proc_name=None):
+    def print_proc_update(self, fields, table_name, proc_name, confirm_proc_name=None):
         autonumber_field = self.get_autonumber_primary_key(fields)
         
         if autonumber_field is None:
@@ -305,3 +306,71 @@ class SGScripter:
             
             print("END " + self.delimiter)
 
+    def print_proc_delete(self, fields, table_name, proc_name):
+            autonumber_field = self.get_autonumber_primary_key(fields)
+            
+            if autonumber_field is None:
+                print("-- Can't generate update procedure without autonumber primary key field.")
+                print()
+            else:
+                tab1 = ' ' * self.tabstop
+
+                param_fields = [autonumber_field]
+
+                autonumber_name = autonumber_field["COLUMN_NAME"]
+                table_alias = table_name[0:1]
+                table_prefix = table_alias + "."
+
+                params_indent_len = self.print_proc_top(proc_name)
+                self.print_proc_params(params_indent_len, param_fields)
+                print("BEGIN")
+
+                delete_str = tab1 + "DELETE FROM "
+                indent_len = len(delete_str)
+
+                delete_target = "{} USING {} AS {}\n".format(table_alias,
+                                                             table_name,
+                                                             table_alias)
+                
+                print(delete_str, end=delete_target)
+
+                # WHERE
+                where_clause = table_alias + "." + autonumber_name + " = " + autonumber_name + ";\n"
+                self.print_to_indent(indent_len, "WHERE ", end=where_clause)
+                print()
+
+                # Report outcome
+                print(tab1 + "SELECT ROW_COUNT() AS deleted;")
+                print("END " + self.delimiter)
+
+    def get_calling_dictionary(self, table, name_prefix):
+        """Generate a dictionary of lists for indirect generation of basic scripts.
+        Args:
+           table (string):      Name of table
+           name_prefix(string): Prefix of procedure names to be generated
+
+        Returns:
+           (dictionary): Mapping of procedure types to list of
+                         values to be used to call a procedure to generate
+                         the type's procedure code.
+        """
+        types = [ "list", "add", "read", "update", "delete" ];
+        dict = {}
+
+        for type in types:
+            method_name = "print_proc_" + type
+            method_reference = getattr(self, method_name)
+            proc_name = name_prefix + "_" + type.capitalize()
+
+            # add and update type procedures always generate the
+            # target record after a successful operation so the client
+            # can update its representation of said record with the
+            # most current version
+            args = [method_reference, table, proc_name]
+            if type == "add" or type == "update":
+                args.append(name_prefix + "_List")
+
+            dict[type] = args
+
+        return dict
+        
